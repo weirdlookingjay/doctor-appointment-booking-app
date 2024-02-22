766cbdbed3373a2d0d0fedab9054d8bee0b60316
Joe Clark <weirdlookingjay@live.com>
2024-02-22 10:45:07 -0500

    Added shadcn & created header component

---
+++ app/_components/Header.jsx
@@ -0,0 +1,47 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  const Menu = [
+    {
+      id: 1,
+      name: "Home",
+      path: "/",
+    },
+    {
+      id: 2,
+      name: "Explorer",
+      path: "/explorer",
+    },
+    {
+      id: 3,
+      name: "Contact Us",
+      path: "/contact",
+    },
+  ];
+  return (
+    <div className="flex items-center justify-between p-4 shadow-sm">
+      <div className="flex items-center gap-10">
+        <Image src="/logo.svg" alt="logo" width={180} height={80} />
+
+        <ul className="md:flex gap-8 hidden">
+          {Menu.map((item, index) => (
+            <Link href={item.path}>
+              <li
+                key={index}
+                className="hover:text-primary cursor-pointer hover:scale-105 transition-all ease-in-out"
+              >
+                {item.name}
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+      <Button>Get Started</Button>
+    </div>
+  );
+};
+
+export default Header;
